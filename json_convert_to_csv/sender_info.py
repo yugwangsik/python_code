21b67cab5b2b800230b12a1710850e09f71f97d4
yugwangsik <gwangsik72@gmail.com>
2022-03-08 11:55:45 +0900

    add

--- json_convert_to_csv/sender_info.py
+++ json_convert_to_csv/sender_info.py
@@ -59,6 +59,7 @@ def save_f(_topic, _data_list, _device):
 
     pyplot.scatter(x_list, y_list)
     pyplot.plot(x_list, y_list)
+    pyplot.tick_params(axis='x', length=1)
     pyplot.title('7c87cedb2940 fw_ver = ma, msg.base_state.curr-temperature')
     pyplot.show()
     pyplot.savefig('data.png')
